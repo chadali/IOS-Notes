b3e540701b2c9156a820277979183ce8527afeab
ali chaudhry <23alic@gmail.com>
2018-06-27 11:34:55 -0400

    Initial Commit

---
+++ Lynda-Notes/DetailViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DetailViewController.swift
+//  Lynda-Notes
+//
+//  Created by ali chaudhry on 6/27/18.
+//  Copyright © 2018 ali chaudhry. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    var text:String = ""
+    var masterView:ViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textView.text = text
+        self.navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    func setText(t:String) {
+        text = t
+        if isViewLoaded {
+            textView.text = t
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        masterView.newRowText = textView.text
+        textView.resignFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
